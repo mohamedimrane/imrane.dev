4739b3f7d246480056a671ddee57e6e5f530301c
Med Imrane <imrane@chehabi.ma>
2020-10-02 00:01:44 +0100

    Little fixes

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <nuxt-link to="/test">test</nuxt-link>
     <AppNavbar id="navbar" class="my-3" />
     <div id="content" class="mb-8 md:my-16 lg:my-20">
       <AboutSection class="mb-6 sm:mb-8 md:mb-16 lg:mb-20" />
--- pages/test.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil ratione nam
-    asperiores minima ipsa, ut velit dicta accusamus quo placeat, iste aut quam.
-    Quibusdam reiciendis natus repellat laboriosam veritatis nobis.
-  </div>
-</template>
