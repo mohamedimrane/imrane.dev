89a1b5c40455f21fcb6dc3a1e2ef79bd4a4ab388
Med Imrane <imrane@chehabi.ma>
2020-10-01 21:01:50 +0100

    404 page fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,6 +25,10 @@ export default {
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: ["@nuxtjs/tailwindcss"],
 
+  generate: {
+    fallback: true
+  },
+
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // https://go.nuxtjs.dev/content
