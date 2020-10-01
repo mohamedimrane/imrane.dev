a02a521e7a45b9c9c8640892a203f87c665726f8
Med Imrane <imrane@chehabi.ma>
2020-10-01 20:33:23 +0100

    Added 404 Page

---
+++ pages/_.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="h-100vh relative">
+    <div
+      class="flex flex-col items-center top-1/2 w-100vw transform -translate-y-1/2 absolute"
+    >
+      <img
+        src="@/assets/images/404.svg"
+        class="w-1/2 mb-2"
+        alt="Page not found"
+      />
+      <h1 class="text-3xl sm:text-4xl md:text-5xl lg:text-6xl">
+        Page not found
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
