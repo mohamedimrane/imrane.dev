2da2f7f691712a593fedc89cc71bd07be86a1d0e
Med Imrane <imrane@chehabi.ma>
2020-10-14 22:33:26 +0100

    Added TailwindCSS to the thanks list

--- components/AppThanks.vue
+++ components/AppThanks.vue
@@ -1,12 +1,21 @@
 <template>
-  <div class="my-2 md:my-4 bg-gray-100 ">
+  <div class="my-2 md:my-4">
     <p class="mx-auto text-center leading-loose">
       Built with
       <a
         href="https://nuxtjs.org/"
         class="prose text-teal-900 transition duration-300 ease-in-out hover:text-teal-700 underline"
-        >NuxtJs</a
-      >.
+      >
+        NuxtJs
+      </a>
+      and
+      <a
+        href="https://tailwindcss.com/"
+        class="prose text-teal-900 transition duration-300 ease-in-out hover:text-teal-700 underline"
+      >
+        TailwindCSS
+      </a>
+      .
       <br />
       A lot of thanks to
       <a
