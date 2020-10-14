f2e517694046f6a270107df91075b88e381d4792
Med Imrane <imrane@chehabi.ma>
2020-10-14 23:43:50 +0100

    Creating a component for a post card

--- components/RecentBlogPostsSection.vue
+++ components/RecentBlogPostsSection.vue
@@ -9,96 +9,32 @@
         </h2>
       </div>
       <div class="flex flex-col lg:flex-row lg:justify-around">
-        <div
-          class="bg-gray-200 w-5/6 mx-auto lg:w-3/12 mb-4 rounded-xl overflow-hidden transition duration-300 ease-in-out hover:shadow-xl"
-        >
-          <div class="relative">
-            <img
-              src="@/assets/images/thumbnails/blog1.png"
-              class="mx-auto"
-              alt="laravel and vue js on a desk"
-            />
-            <p
-              class="bg-red-600 text-red-100 md:text-lg lg:text-base p-2 absolute rounded-tr-lg bottom-0 left-0 font-varela-round"
-            >
-              Laravel
-            </p>
-          </div>
-          <div class="pb-5 pt-1 px-6 bg-red-600 text-red-100 font-quicksand">
-            <h2 class="text-xl md:text-2xl font-bold leading-loose">
-              Lorem ipsum Dolore Sit Amet
-            </h2>
-            <p class="md:text-lg lg:text-lg leading-loose">
-              Say what your post is about
-            </p>
-            <span class="text-xs leading-loose">
-              <span class="font-bold">18 August 2020 &bull; </span>
-              Imrane Chehabi
-            </span>
-          </div>
-        </div>
-        <div
-          class="bg-gray-200 w-5/6 mx-auto lg:w-3/12 mb-4 rounded-xl overflow-hidden transition duration-300 ease-in-out hover:shadow-xl"
-        >
-          <div class="relative">
-            <img
-              src="@/assets/images/thumbnails/blog1.png"
-              class="mx-auto"
-              alt="laravel and vue js on a desk"
-            />
-            <p
-              class="bg-green-500 text-green-100 md:text-lg lg:text-base p-2 absolute rounded-tr-lg bottom-0 left-0 font-varela-round"
-            >
-              Vue.js
-            </p>
-          </div>
-          <div
-            class="pb-5 pt-1 px-6 bg-green-500 text-green-100 font-quicksand"
-          >
-            <h2 class="text-xl md:text-2xl font-bold leading-loose">
-              Lorem ipsum Dolore Sit Amet
-            </h2>
-            <p class="md:text-lg lg:text-lg leading-loose">
-              Say what your post is about
-            </p>
-            <span class="text-xs leading-loose">
-              <span class="font-bold">18 August 2020 &bull; </span>
-              Imrane Chehabi
-            </span>
-          </div>
-        </div>
-        <div
-          class="bg-gray-200 w-5/6 mx-auto lg:w-3/12 mb-4 rounded-xl overflow-hidden transition duration-300 ease-in-out hover:shadow-xl"
-        >
-          <div class="relative">
-            <img
-              src="@/assets/images/thumbnails/blog1.png"
-              class="mx-auto"
-              alt="laravel and vue js on a desk"
-            />
-            <p
-              class="bg-teal-600 text-teal-100 md:text-lg lg:text-base p-2 absolute rounded-tr-lg bottom-0 left-0 font-varela-round"
-            >
-              Tailwind CSS
-            </p>
-          </div>
-          <div class="relative">
-            <div
-              class="pb-5 pt-1 px-6 bg-teal-600 text-teal-100 font-quicksand"
-            >
-              <h2 class="text-xl md:text-2xl font-bold leading-loose">
-                Lorem ipsum Dolore Sit Amet
-              </h2>
-              <p class="md:text-lg lg:text-lg leading-loose">
-                Say what your post is about
-              </p>
-              <span class="text-xs leading-loose">
-                <span class="font-bold">18 August 2020 &bull;</span> Imrane
-                Chehabi
-              </span>
-            </div>
-          </div>
-        </div>
+        <AppPostCard
+          category="Laravel"
+          cardColor="red"
+          title="Lorem Ipsum Dolor Sit Amet"
+          description="Say what your post is about"
+          createdAt="18 August 2020"
+          author="Imrane Chehabi"
+        />
+
+        <AppPostCard
+          category="Vue.js"
+          cardColor="green"
+          title="Lorem Ipsum Dolor Sit Amet"
+          description="Say what your post is about"
+          createdAt="18 August 2020"
+          author="Imrane Chehabi"
+        />
+
+        <AppPostCard
+          category="TailwindCSS"
+          cardColor="teal"
+          title="Lorem Ipsum Dolor Sit Amet"
+          description="Say what your post is about"
+          createdAt="18 August 2020"
+          author="Imrane Chehabi"
+        />
       </div>
     </div>
   </div>
