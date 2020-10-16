82e36865101e1b9e207576838deee74d064a60da
Med Imrane <imrane@chehabi.ma>
2020-10-16 01:22:55 +0100

    Fixed links

--- components/navbar/NavbarLinks.vue
+++ components/navbar/NavbarLinks.vue
@@ -2,42 +2,64 @@
   <ul
     class="hidden text-2xl text-gray-700 md:flex md:flex-row md:justify-between"
   >
-    <li
-      v-scroll-to="{
-        el: '#about',
-        onDone: () => {
-          whenScrollignToSectionIsDone('about')
-        },
-      }"
-      tag="li"
-      class="mx-8 transition duration-300 ease-in-out hover:text-teal-600 cursor-pointer"
-    >
-      About
-    </li>
-    <li
-      v-scroll-to="{
-        el: '#skills',
-        onDone: () => {
-          whenScrollignToSectionIsDone('skills')
-        },
-      }"
-      tag="li"
-      class="mx-8 transition duration-300 ease-in-out hover:text-teal-600 cursor-pointer"
-    >
-      Skills
-    </li>
-    <li
-      v-scroll-to="{
-        el: '#recent-blog-posts',
-        onDone: () => {
-          whenScrollignToSectionIsDone('recent-blog-posts')
-        },
-      }"
-      tag="li"
-      class="mx-8 transition duration-300 ease-in-out hover:text-teal-600 cursor-pointer"
-    >
-      Blog
-    </li>
+    <template v-if="pageIsHome">
+      <li
+        v-scroll-to="{
+          el: '#about',
+          onDone: () => {
+            whenScrollignToSectionIsDone('about')
+          },
+        }"
+        class="mx-8 transition duration-300 ease-in-out hover:text-teal-600 cursor-pointer"
+      >
+        About
+      </li>
+      <li
+        v-scroll-to="{
+          el: '#skills',
+          onDone: () => {
+            whenScrollignToSectionIsDone('skills')
+          },
+        }"
+        class="mx-8 transition duration-300 ease-in-out hover:text-teal-600 cursor-pointer"
+      >
+        Skills
+      </li>
+      <li
+        v-scroll-to="{
+          el: '#recent-blog-posts',
+          onDone: () => {
+            whenScrollignToSectionIsDone('recent-blog-posts')
+          },
+        }"
+        class="mx-8 transition duration-300 ease-in-out hover:text-teal-600 cursor-pointer"
+      >
+        Blog
+      </li>
+    </template>
+    <template v-if="!pageIsHome">
+      <nuxt-link
+        tag="li"
+        to="/#about"
+        class="mx-8 transition duration-300 ease-in-out hover:text-teal-600 cursor-pointer"
+      >
+        <a>About</a>
+      </nuxt-link>
+      <nuxt-link
+        tag="li"
+        to="/#skills"
+        class="mx-8 transition duration-300 ease-in-out hover:text-teal-600 cursor-pointer"
+      >
+        <a>Skills</a>
+      </nuxt-link>
+      <nuxt-link
+        tag="li"
+        to="/#recent-blog-posts"
+        class="mx-8 transition duration-300 ease-in-out hover:text-teal-600 cursor-pointer"
+      >
+        <a>Blog</a>
+      </nuxt-link>
+    </template>
   </ul>
 </template>
 
@@ -46,6 +68,11 @@ export default {
   methods: {
     whenScrollignToSectionIsDone(section) {
       this.$router.push('/#' + section)
+    },
+  },
+  computed: {
+    pageIsHome() {
+      return this.$route.path === '/'
     },
   },
 }
--- components/navbar/NavbarLinksHiddenable.vue
+++ components/navbar/NavbarLinksHiddenable.vue
@@ -1,41 +1,63 @@
 <template>
   <ul class="absolute w-full top-14 -z-1 bg-white text-2xl text-gray-700">
-    <li
-      v-scroll-to="{
-        el: '#about',
-        onDone: () => {
-          whenScrollignToSectionIsDone('about')
-        },
-      }"
-      tag="li"
-      class="mx-8 transition duration-300 ease-in-out hover:text-teal-600 cursor-pointer"
-    >
-      About
-    </li>
-    <li
-      v-scroll-to="{
-        el: '#skills',
-        onDone: () => {
-          whenScrollignToSectionIsDone('skills')
-        },
-      }"
-      tag="li"
-      class="mx-8 transition duration-300 ease-in-out hover:text-teal-600 cursor-pointer"
-    >
-      Skills
-    </li>
-    <li
-      v-scroll-to="{
-        el: '#recent-blog-posts',
-        onDone: () => {
-          whenScrollignToSectionIsDone('recent-blog-posts')
-        },
-      }"
-      tag="li"
-      class="mx-8 transition duration-300 ease-in-out hover:text-teal-600 cursor-pointer"
-    >
-      Blog
-    </li>
+    <template v-if="pageIsHome">
+      <li
+        v-scroll-to="{
+          el: '#about',
+          onDone: () => {
+            whenScrollignToSectionIsDone('about')
+          },
+        }"
+        class="mx-8 transition duration-300 ease-in-out hover:text-teal-600 cursor-pointer"
+      >
+        About
+      </li>
+      <li
+        v-scroll-to="{
+          el: '#skills',
+          onDone: () => {
+            whenScrollignToSectionIsDone('skills')
+          },
+        }"
+        class="mx-8 transition duration-300 ease-in-out hover:text-teal-600 cursor-pointer"
+      >
+        Skills
+      </li>
+      <li
+        v-scroll-to="{
+          el: '#recent-blog-posts',
+          onDone: () => {
+            whenScrollignToSectionIsDone('recent-blog-posts')
+          },
+        }"
+        class="mx-8 transition duration-300 ease-in-out hover:text-teal-600 cursor-pointer"
+      >
+        Blog
+      </li>
+    </template>
+    <template v-if="!pageIsHome">
+      <nuxt-link
+        tag="li"
+        to="/#about"
+        class="mx-8 transition duration-300 ease-in-out hover:text-teal-600 cursor-pointer"
+      >
+        <a>About</a>
+      </nuxt-link>
+      <nuxt-link
+        tag="li"
+        to="/#skills"
+        class="mx-8 transition duration-300 ease-in-out hover:text-teal-600 cursor-pointer"
+      >
+        <a>Skills</a>
+      </nuxt-link>
+      <nuxt-link
+        tag="li"
+        to="/#recent-blog-posts"
+        class="mx-8 transition duration-300 ease-in-out hover:text-teal-600 cursor-pointer"
+      >
+        <a>Blog</a>
+      </nuxt-link>
+    </template>
   </ul>
 </template>
 
@@ -62,6 +84,11 @@ export default {
   methods: {
     whenScrollignToSectionIsDone(section) {
       this.$router.push('/#' + section)
+    },
+  },
+  computed: {
+    pageIsHome() {
+      return this.$route.path === '/'
     },
   },
 }
