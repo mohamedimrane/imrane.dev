00f139a0f0716d7fd5875b45edd0d07c36b675bc
Med Imrane <imrane@chehabi.ma>
2020-10-16 01:21:39 +0100

    Fixed about section text size on small screens

--- components/AboutSection.vue
+++ components/AboutSection.vue
@@ -3,11 +3,13 @@
     <div class="mx-auto my-4 px-4 sm:px-0 container flex flex-col items-center">
       <Avatar class="mb-5" />
       <div class="flex flex-col items-center">
-        <h1 class="font-fredoka-one text-5xl lg:text-6xl text-gray-900">
+        <h1
+          class="font-fredoka-one text-4xl sm:text-5xl lg:text-6xl text-gray-900"
+        >
           Imrane Chehabi
         </h1>
         <h3
-          class="font-quicksand font-medium text-xl lg:text-2xl text-gray-600"
+          class="font-quicksand font-medium text-lg sm:text-xl lg:text-2xl text-gray-600"
         >
           Web Desiner & Fullstack Developer
         </h3>
@@ -19,7 +21,7 @@
             class="-m-3 p-5 w-3/4 border border-gray-500 bg-white shadow-xl rounded-xl"
           >
             <p
-              class="font-quicksand text-lg lg:text-xl text-gray-600 md:leading-6 lg:leading-8"
+              class="font-quicksand text-base sm:text-lg lg:text-xl text-gray-600 md:leading-6 lg:leading-8"
             >
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet
               doloremque praesentium deleniti quidem laborum?
