e9d4d5fd258f7b0b3790943ca8ac55dc1c628836
Med Imrane <imrane@chehabi.ma>
2020-12-23 16:05:22 +0100

    Add the 'about-me' section

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,12 @@
 <template>
   <Layout>
-    <section>
+    <section id="about-me" class="flex justify-between">
       <div>
-        <h1 class="text-natural-black">Imrane Chehabi</h1>
-        <h2>Web Designer and Fullstack Developer</h2>
-        <p>Hey! I'am Mohamed Imrane Chehabi, a 12 years old programmer who works with modern technologies like Laravel, October CMS, Vue.js, Nuxt.js, Gridsome and Tailwind CSS.</p>
+        <h1 class="text-5xl text-natural-black font-fredoka-one">Imrane Chehabi</h1>
+        <h2 class="mt-2 text-2xl font-bold text-gray-600 font-nunito-sans">Web Designer and Fullstack Developer</h2>
+        <p class="mt-6 text-natural-black font-nunito-sans">Hey! I'am Mohamed Imrane Chehabi, a 12 years old programmer who works with modern technologies like Laravel, October CMS, Vue.js, Nuxt.js, Gridsome and Tailwind CSS. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos earum quasi, corporis eos consequuntur molestias reprehenderit exercitationem quod tempora tempore.</p>
       </div>
+      <g-image src="~/assets/illustrations/web-developer.svg" alt=""  class="ml-10"/>
     </section>
   </Layout>
 </template>
