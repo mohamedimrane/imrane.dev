275d6401b454e76a49c662791e50558a84ad4fe7
Med Imrane <imrane@chehabi.ma>
2020-12-26 02:01:11 +0100

    Register the blog layout in the project

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Default.vue'
+import BlogLayout from '~/layouts/Blog.vue'
 
 import "fontsource-nunito-sans/latin-400.css" // Normal
 import "fontsource-nunito-sans/latin-600.css" // Semibold
@@ -12,4 +13,5 @@ import "fontsource-fredoka-one";
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+  Vue.component('BlogLayout', BlogLayout)
 }
