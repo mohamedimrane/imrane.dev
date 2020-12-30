b4c9c7184da5bc0ffd30cc8ad6ba5f4a8e025099
Med Imrane <imrane@chehabi.ma>
2020-12-30 15:31:48 +0100

    Create a layout for tags

---
+++ src/layouts/Tag.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex flex-col justify-between min-h-screen font-nunito-sans">
+    <div>
+      <AppHeader />
+  
+      <main class="w-5/12 mx-auto mt-32">
+        <slot />
+      </main>
+    </div>
+
+    <AppFooter />
+  </div>
+</template>
