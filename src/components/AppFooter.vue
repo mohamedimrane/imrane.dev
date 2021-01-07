784f094e74fab8e1e5e92f5741b20df02e7fb1dd
Med Imrane <imrane@chehabi.ma>
2021-01-07 23:49:36 +0100

    Make the footer mobile responsive

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,14 +1,14 @@
 <template>
-  <footer class="py-5 bg-gray-200 mt-28">
-      <div class="flex justify-between w-4/6 mx-auto">
-        <span class="font-semibold">
+  <footer class="py-5 mt-12 bg-gray-200 md:mt-28">
+      <div class="flex flex-col justify-between w-4/6 mx-auto md:flex-row">
+        <span class="text-sm font-semibold text-center md:text-base">
           Built with <a href="https://gridsome.org/" class="text-green-500 cursor-pointer">Gridsome</a>. 
           Hosted on <a href="https://www.netlify.com/" class="cursor-pointer text-teal">Netlify</a> and <a href="https://github.com/" class="cursor-pointer text-teal">Github</a>.
         </span>
 
-        <div class="flex justify-between w-1/12">
+        <div class="flex justify-between mx-auto mt-5 md:mx-0 md:mt-0">
           <a href="https://twitter.com/ImraneChehabi"><g-image src="~/assets/logos/twitter.svg" /></a>
-          <a href="https://github.com/mohamedimrane"><g-image src="~/assets/logos/github.svg" /></a>
+          <a class="ml-6" href="https://github.com/mohamedimrane"><g-image src="~/assets/logos/github.svg" /></a>
         </div>
       </div>
     </footer>
