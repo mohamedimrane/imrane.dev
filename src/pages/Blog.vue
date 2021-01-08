ee9c3abe73c13310c42ba61d3dc201bc92759b16
Med Imrane <imrane@chehabi.ma>
2021-01-08 10:09:50 +0100

    Make the blog page mobile responsive

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -1,8 +1,8 @@
 <template>
   <BlogLayout>
     <h2 class="text-4xl font-bold text-natural-black">Articles</h2>
-    <div class="flex justify-between mt-10">
-      <div class="relative w-1/3 mr-8">
+    <div class="flex flex-col justify-between mt-10 md:flex-row">
+      <div class="relative w-full mr-8 md:w-1/3">
         <input type="text" id="search-bar" placeholder="Search" class="w-full h-10 pl-10 pr-3 text-lg border border-opacity-50 rounded focus:outline-none focus:ring-1 placeholder-blue50 border-light-text-200 text-natural-black">
         <div class="absolute stroke-current left-2 top-1.5 text-blue50">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor"
@@ -14,7 +14,7 @@
         </div>
       </div>
 
-      <section id="articles" class="w-2/3">
+      <section id="articles" class="w-full mt-4 md:mt-0 md:w-2/3">
         <ArticleSnippet v-for="edge in $page.articles.edges" :key="edge.title" :article="edge.node" />
       </section>
     </div>
