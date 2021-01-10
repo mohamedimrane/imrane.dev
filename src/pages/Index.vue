5d02f6d9edbad42ca44d393c29e9d0858629bb4c
Med Imrane <imrane@chehabi.ma>
2021-01-10 23:52:22 +0100

    Make the 'contact-me' section tablet responsive

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -22,14 +22,14 @@
     </section>
 
     <section id="contact-me" class="mt-12 md:mt-28">
-      <h2 class="text-2xl font-bold text-center md:text-4xl text-natural-black">Contact Me</h2>
-      <h3 class="mt-1 text-lg md:mt-10 md:text-2xl text-natural-black">Looking to start a project?</h3>
-      <h4 class="text-gray-400 md:mt-2 md:text-lg">Use the form below</h4>
-      <div class="md:grid md:grid-cols-2">
+      <h2 class="text-2xl font-bold text-center md:text-3xl lg:text-4xl text-natural-black">Contact Me</h2>
+      <h3 class="mt-1 text-lg md:mt-10 md:text-xl lg:text-2xl text-natural-black">Looking to start a project?</h3>
+      <h4 class="text-gray-400 lg:mt-2 lg:text-lg">Use the form below</h4>
+      <div class="lg:grid lg:grid-cols-2">
         <form @click.prevent="" class="mt-5">
-          <div class="md:grid md:grid-cols-2 md:gap-x-4">
+          <div class="lg:grid lg:grid-cols-2 lg:gap-x-4">
             <input type="text" name="full_name" id="full_name" placeholder="Full Name" class="w-full p-2 text-gray-800 border border-gray-400 rounded-md shadow focus:outline-none focus:ring-2 placeholder-opacity-40">
-            <input type="email" name="email" id="email" placeholder="Email" class="w-full p-2 mt-3 text-gray-800 border border-gray-400 rounded-md shadow md:mt-0 focus:outline-none focus:ring-2 placeholder-opacity-40">
+            <input type="email" name="email" id="email" placeholder="Email" class="w-full p-2 mt-3 text-gray-800 border border-gray-400 rounded-md shadow md:mt-6 lg:mt-0 focus:outline-none focus:ring-2 placeholder-opacity-40">
           </div>
           <input type="text" name="subject" id="subject" placeholder="Subject" class="w-full p-2 mt-3 text-gray-800 border border-gray-400 rounded-md shadow md:mt-6 focus:outline-none focus:ring-2 placeholder-opacity-40">
           <textarea name="message" id="message" placeholder="Message" class="w-full h-40 p-2 mt-3 text-gray-800 border border-gray-400 rounded-md shadow md:mt-6 focus:outline-none focus:ring-2 placeholder-opacity-40"></textarea>
@@ -37,7 +37,7 @@
             <g-image class="mt-0.5" src="~/assets/icons/send.svg"/> <span class="ml-3">Send Message</span>
           </button>
         </form>
-        <g-image class="hidden my-auto md:block" src="~/assets/illustrations/paper-plane.svg" alt="" />
+        <g-image class="hidden my-auto lg:block" src="~/assets/illustrations/paper-plane.svg" alt="" />
       </div>
     </section>
   </Layout>
