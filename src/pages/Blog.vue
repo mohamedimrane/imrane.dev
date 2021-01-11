39df71e53d3e935d3ffbcbbc8b9671df0cd6225a
Med Imrane <imrane@chehabi.ma>
2021-01-11 16:44:13 +0100

    Make the blog page tablet responsive

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -1,8 +1,8 @@
 <template>
   <BlogLayout>
-    <h2 class="text-3xl font-bold md:text-4xl text-natural-black">Articles</h2>
-    <div class="flex flex-col justify-between mt-6 md:mt-10 md:flex-row">
-      <div class="relative w-full mr-8 md:w-1/3">
+    <h2 class="text-3xl font-bold lg:text-4xl text-natural-black">Articles</h2>
+    <div class="flex flex-col justify-between mt-6 lg:mt-10 lg:flex-row">
+      <div class="relative w-full mr-8 lg:w-1/3">
         <input type="text" id="search-bar" placeholder="Search" class="w-full h-10 pl-10 pr-3 text-lg border border-opacity-50 rounded focus:outline-none focus:ring-1 placeholder-blue50 border-light-text-200 text-natural-black">
         <div class="absolute stroke-current left-2 top-1.5 text-blue50">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor"
@@ -14,7 +14,7 @@
         </div>
       </div>
 
-      <section id="articles" class="w-full mt-4 md:mt-0 md:w-2/3">
+      <section id="articles" class="w-full mt-4 lg:mt-0 lg:w-2/3">
         <ArticleSnippet v-for="edge in $page.articles.edges" :key="edge.title" :article="edge.node" />
       </section>
     </div>
