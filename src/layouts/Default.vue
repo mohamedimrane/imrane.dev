ceb9444993075c0bfbd725ce24910e1568d67c1a
Med Imrane <imrane@chehabi.ma>
2021-01-13 15:56:05 +0100

    Add the toggle theme's logic to the default layout

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,13 +1,30 @@
 <template>
-  <div class="flex flex-col justify-between min-h-screen font-nunito-sans">
-    <div>
-      <AppHeader />
+  <div :class="{ 'dark': theme === 'dark' }">
+    <div class="flex flex-col justify-between min-h-screen font-nunito-sans">
+      <div>
+        <AppHeader @themeSwitched="switchTheme" :theme="theme"/>
+    
+        <main class="w-11/12 mx-auto mt-16 md:w-8/12 lg:w-6/12 lg:mt-32">
+          <slot />
+        </main>
+      </div>
   
-      <main class="w-11/12 mx-auto mt-16 md:w-8/12 lg:w-6/12 lg:mt-32">
-        <slot />
-      </main>
+      <AppFooter />
     </div>
-
-    <AppFooter />
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return { 
+      theme: 'light'
+    }
+  },
+  methods: {
+    switchTheme (theme) {
+      this.theme = theme
+    }
+  }
+};
+</script>
