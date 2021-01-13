3e35b9b600be3cdf1a95defee3c88af951ac8161
Med Imrane <imrane@chehabi.ma>
2021-01-13 14:49:26 +0100

    Add the theme toggle button

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,7 +7,41 @@
         <g-link to="/" tag="li" class="font-bold uppercase cursor-pointer text-natural-black">ABOUT</g-link>
         <g-link to="/#skills" class="font-bold uppercase cursor-pointer ml-7 md:ml-20 lg:ml-0 text-natural-black">SKILLS</g-link>
         <g-link to="/blog" tag="li" class="font-bold uppercase cursor-pointer ml-7 md:ml-20 lg:ml-0 text-natural-black">BLOG</g-link>
+        <li class="cursor-pointer ml-7 md:ml-20 lg:ml-0"><input id="toggle" class="toggle" type="checkbox" checked></li>
       </ul>
     </nav>
   </header>
 </template>
+
+<style>
+.toggle {
+   --size: 1.4rem;
+   appearance: none;
+   outline: none;
+   cursor: pointer;
+   width: var(--size);
+   height: var(--size);
+   box-shadow: inset calc(var(--size) * 0.33) calc(var(--size) * -0.25) 0;
+   border-radius: 999px;
+   color: #e6e6ff;
+   transition: all 500ms;
+}
+ .toggle:checked {
+   --ray-size: calc(var(--size) * -0.4);
+   --offset-orthogonal: calc(var(--size) * 0.65);
+   --offset-diagonal: calc(var(--size) * 0.45);
+   transform: scale(0.75);
+   color: #fa0;
+   box-shadow: inset 0 0 0 var(--size), calc(var(--offset-orthogonal) * -1) 0 0 var(--ray-size), var(--offset-orthogonal) 0 0 var(--ray-size), 0 calc(var(--offset-orthogonal) * -1) 0 var(--ray-size), 0 var(--offset-orthogonal) 0 var(--ray-size), calc(var(--offset-diagonal) * -1) calc(var(--offset-diagonal) * -1) 0 var(--ray-size), var(--offset-diagonal) var(--offset-diagonal) 0 var(--ray-size), calc(var(--offset-diagonal) * -1) var(--offset-diagonal) 0 var(--ray-size), var(--offset-diagonal) calc(var(--offset-diagonal) * -1) 0 var(--ray-size);
+}
+
+ .toggle {
+   z-index: 1;
+}
+ .toggle:checked ~ .background {
+   --bg: white;
+}
+ .toggle:checked ~ .title {
+   --color: #fa0;
+}
+</style>
