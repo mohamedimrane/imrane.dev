153a46ea6fef7f2cca35f46463e5b13eb2148f4b
Med Imrane <imrane@chehabi.ma>
2021-01-13 23:23:58 +0100

    Add dark mode to the footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,12 +1,12 @@
 <template>
-  <footer class="py-5 mt-12 bg-gray-200 md:mt-28">
+  <footer class="py-5 mt-12 bg-gray-200 dark:bg-darkmode-dark-teal md:mt-28">
       <div class="flex flex-col justify-between w-4/6 mx-auto md:flex-row">
-        <span class="text-sm font-semibold text-center md:text-base">
+        <span class="text-sm font-semibold text-center md:text-base dark:text-white">
           Built with <a href="https://gridsome.org/" class="text-green-500 cursor-pointer">Gridsome</a>. 
           Hosted on <a href="https://www.netlify.com/" class="cursor-pointer text-teal">Netlify</a> and <a href="https://github.com/" class="cursor-pointer text-teal">Github</a>.
         </span>
 
-        <div class="flex justify-between mx-auto mt-5 md:mx-0 md:mt-0">
+        <div class="flex items-center justify-between mx-auto mt-5 md:mx-0 md:mt-0">
           <a href="https://twitter.com/ImraneChehabi"><g-image src="~/assets/logos/twitter.svg" /></a>
           <a class="ml-6" href="https://github.com/mohamedimrane"><g-image src="~/assets/logos/github.svg" /></a>
         </div>
