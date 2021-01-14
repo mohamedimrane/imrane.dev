adac00760a1b779531a8edfed46641256beb55fd
Med Imrane <imrane@chehabi.ma>
2021-01-14 22:34:13 +0100

    Fix darkmode syling of the articles' search widget

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -15,13 +15,13 @@
       </div>
       <div v-if="searchResults.length > 0" class="absolute left-0 right-0 z-10 mt-2 mb-4 overflow-hidden overflow-y-auto text-left normal-case bg-white border rounded shadow-sm" style="max-height: 32rem">
         <div class="flex flex-col">
-          <g-link v-for="result in searchResults" :key="result.id" :to="result.path" class="p-4 font-semibold border-b cursor-pointer dark:bg-darkmode-dark-teal dark:text-white border-light-text10 hover:bg-light-text10">
+          <g-link v-for="result in searchResults" :key="result.id" :to="result.path" class="p-4 font-semibold border-b cursor-pointer text-natural-black dark:bg-darkmode-dark-teal dark:text-white border-light-text10 hover:bg-light-text10">
             {{ result.title }}
             <span class="block my-1 text-sm font-normal text-blue40">{{ result.node.summary }}</span>
           </g-link>
         </div>
       </div>
-      <div v-if="searchResults.length === 0 & searchTerm.length > 2" class="w-full p-4 mt-1 font-normal border-b cursor-pointer bg-light-text10">
+      <div v-if="searchResults.length === 0 & searchTerm.length > 2" class="w-full p-4 mt-1 font-normal border-b cursor-pointer bg-light-text10 dark:text-white text-natural-black">
         <p class="my-0">
           No results for '<strong>{{ searchTerm }}</strong>'
         </p>
