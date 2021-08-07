05b5891e6212d3fe45ab658157104af3ae5bd0ac
Med Imrane <imrane@chehabi.ma>
2021-08-07 22:12:54 +0100

    Fix "latest blog posts" section

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -16,7 +16,7 @@
     <!-- Start Latest blog posts  -->
     <div>
       <svg class="w-full text-primary fill-current" viewBox="0 0 1400 50" xmlns="http://www.w3.org/2000/svg"><path d="M0 0c309.151 33.333 542.484 50 700 50 157.516 0 390.849-16.667 700-50v50H0V0z"></path></svg>
-      <div class="py-20 bg-primary text-white">
+      <div class="py-40 bg-primary text-white">
         <div class="container">
           <h1 class="text-4xl font-extrabold">Latest Blog Posts</h1>
           <div class="mt-10">
@@ -26,6 +26,7 @@
           </div>
         </div>
       </div>
+      <svg class="w-full text-primary fill-current" viewBox="0 0 1400 50" xmlns="http://www.w3.org/2000/svg"><path d="M0 50C309.151 16.667 542.484 0 700 0c157.516 0 390.849 16.667 700 50V0H0v50z"></path></svg>
     </div>
     <!-- End Latest blog posts -->
   </Layout>
