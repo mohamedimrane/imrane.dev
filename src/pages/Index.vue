e06d4acc4716b4cbca2fc4a0d1619e89c6b21d81
Med Imrane <imrane@chehabi.ma>
2021-08-07 22:10:00 +0100

    Add "about me"

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,5 +1,17 @@
 <template>
   <Layout>
+    <!-- Start About me -->
+    <div class="container mt-60 grid grid-cols-2 gap-x-20">
+      <div>
+        <div class="font-extrabold">
+          <h1 class="text-6xl">Mohamed Imrane Chehabi</h1>
+          <h2 class="mt-1 text-2xl text-primary">Full Stack Web Developer</h2>
+        </div>
+        <p class="mt-6 text-lg text-secondary-light">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas dolore ullam maiores odit explicabo dolorum velit adipisci officiis quis at.</p>
+      </div>
+      <g-image src="~/assets/illustrations/developer.svg"></g-image>
+    </div>
+    <!-- End About me -->
   </Layout>
 </template>
 
