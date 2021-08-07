ee6a99f22ba88afeb3030c817a95a16f12ab0c06
Med Imrane <imrane@chehabi.ma>
2021-08-07 22:10:34 +0100

    Add "latest blog posts" section

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -12,8 +12,43 @@
       <g-image src="~/assets/illustrations/developer.svg"></g-image>
     </div>
     <!-- End About me -->
+
+    <!-- Start Latest blog posts  -->
+    <div>
+      <svg class="w-full text-primary fill-current" viewBox="0 0 1400 50" xmlns="http://www.w3.org/2000/svg"><path d="M0 0c309.151 33.333 542.484 50 700 50 157.516 0 390.849-16.667 700-50v50H0V0z"></path></svg>
+      <div class="py-20 bg-primary text-white">
+        <div class="container">
+          <h1 class="text-4xl font-extrabold">Latest Blog Posts</h1>
+          <div class="mt-10">
+            <div class="grid grid-cols-2 gap-y-4 gap-x-10">
+              <a class="text-xl font-bold block hover:underline" v-for="article in $page.articles.edges" :key="article.node.id" :href="article.node.path">{{ article.node.title }}</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- End Latest blog posts -->
   </Layout>
 </template>
+
+<page-query>
+query Article {
+  articles: allArticle {
+    edges {
+      node {
+        title
+        id
+        path
+        icon {
+          path
+          alt
+        }
+        date (format: "MMMM D, Y")
+      }
+    }
+  }
+}
+</page-query>
 
 <script>
 export default {
