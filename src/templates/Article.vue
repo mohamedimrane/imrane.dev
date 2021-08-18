59075f84e9e19e3c7f98b75fdced47e4d9da1f27
Med Imrane <imrane@chehabi.ma>
2021-08-18 18:46:28 +0100

    Adapt articles template to the design v2

--- src/templates/Article.vue
+++ src/templates/Article.vue
@@ -1,19 +1,18 @@
 <template>
-  <ArticleLayout>
-    <main class="mx-auto mt-16 lg:mt-32 container-inner">
-      <h1 class="text-2xl font-bold text-natural-black dark:text-white lg:text-3xl">{{ $page.article.title }}</h1>
-      <p class="mt-2 text-sm lg:mt-4 lg:text-base text-blue40 dark:text-gray-500">Last update on <time :datetime="$page.article.datetime">{{ $page.article.date }}</time></p>
-      <ul class="mt-2">
-        <g-link tag="li" class="inline-block px-3 py-1 mr-3 text-sm bg-gray-400 bg-opacity-25 rounded cursor-pointer dark:text-white hover:bg-opacity-40"
-          v-for="tag in $page.article.tags"
-          :key="tag.id" :to="tag.path"
-        >{{ stringCapitalize(tag.title) }}</g-link>
-      </ul>
-      <div class="mt-8 markdown-body">
+  <Layout>
+    <main class="container mt-20">
+      <div class="space-y-2">
+        <h1 class="text-3xl font-bold">{{ $page.article.title }}</h1>
+        <p class="">Last update on <time :datetime="$page.article.datetime">{{ $page.article.date }}</time></p>
+        <div class="text-primary font-semibold">
+          <g-link class="uppercase text-xs hover:bg-primary hover:text-white py-1 px-4 rounded-full transition duration-100 ease-in-out" v-for="tag in $page.article.tags" :key="tag.id" :to="tag.path">{{ tag.title }}</g-link>
+        </div>
+      </div>
+      <div class="markdown-body">
         <VueRemarkContent class="leading-9 tracking-wide" />
       </div>
     </main>
-  </ArticleLayout>
+  </Layout>
 </template>
 
 <page-query>
@@ -93,3 +92,5 @@ export default {
   }
 };
 </script>
+
+<style src="../github-markdown.css" />
