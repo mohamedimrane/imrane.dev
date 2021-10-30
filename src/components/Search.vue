8e9e979c7627ecfa58c7d10b429f0945036604a4
Med Imrane <imrane@chehabi.ma>
2021-10-31 00:18:57 +0100

    Makes the x icon in the blog search bar look nicer

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -5,7 +5,9 @@
       <div class="relative">
         <input type="text" id="search-bar" placeholder="Search" v-model="searchTerm" class="w-full text-sm pr-8 pl-8 bg-white border-2 border-primary focus:border-primary rounded-lg transform -translate-x-1 -translate-y-1 focus:ring-0">
         <svg class="text-secondary-light fill-current absolute w-4 h-4 top-0 left-2 top-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"/></svg>
-        <svg v-if="searchTerm.length > 0" class="text-secondary-light fill-current absolute w-6 h-6 top-1.5 right-2" @click="reset" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+        <div v-if="searchTerm.length > 0" @click="reset" class="absolute top-2.5 right-2.5">
+          <svg class="text-secondary-light fill-current w-4 h-4" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" width="22" height="32" preserveAspectRatio="xMidYMid meet" viewBox="0 0 352 512"><path d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28L75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256L9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z" fill="currentColor"></path></svg>
+        </div>
       </div>
     </div>
     <!-- End Input -->
