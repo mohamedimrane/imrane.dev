a414e43818c969ece90cdf8e20f6fbae00b2ec47
Mohamed Imrane Chehabi <38333092+mohamedimrane@users.noreply.github.com>
2022-02-12 03:28:45 +0100

    Fixes the article snippet

--- components/blog/BlogArticleSnippet.vue
+++ components/blog/BlogArticleSnippet.vue
@@ -2,13 +2,13 @@
   <nuxt-link tag="div" :to="'blog/article' + article.path" class="group bg-primary rounded-lg transform translate-x-1 translate-y-1 transform hover:scale-102 transition-transform duration-100 ease-in-out cursor-pointer">
     <div class="flex p-6 bg-white border-2 border-primary rounded-lg transform -translate-x-1 -translate-y-1">
       <div class="w-1/12 flex items-center">
-        <img :src="article.icon.path" class="w-10 h-10" />
+        <img :src="'/' + article.icon.path" class="w-10 h-10" />
       </div>
       <div class="w-11/12">
         <h4 class="block text-lg font-bold">{{ article.title }}</h4>
         <time class="block text-sm text-secondary-light" :datetime="article.updatedAt">{{ articleFormatedUpdatedAt }}</time>
         <div class="text-primary font-semibold -ml-2">
-          <nuxt-link class="uppercase text-xs hover:bg-primary hover:text-white py-1 px-2 rounded-full transition duration-100 ease-in-out" v-for="tag in article.tags" :key="tag.id" :to="tag.path">{{ tag.title }}</nuxt-link>
+          <nuxt-link class="uppercase text-xs hover:bg-primary hover:text-white py-1 px-2 rounded-full transition duration-100 ease-in-out" v-for="tag in article.tags" :key="tag.id" :to="'blog/tag' + tag.path">{{ tag.title }}</nuxt-link>
         </div>
       </div>
     </div>
