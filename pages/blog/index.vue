6a92868cb277d409fadd94eb66dc65a9717bf707
Mohamed Imrane Chehabi <38333092+mohamedimrane@users.noreply.github.com>
2022-05-01 16:24:06 +0000

    feat: :sparkles: Makes the blog index page md responsive

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="container mb-40 mt-20 flex flex-col">
+    <div class="container mb-20 lg:mb-40 mt-20 flex flex-col">
       <!-- Start Title -->
       <h2 class="text-3xl font-extrabold relative flex items-center self-start ml-2">
         <span class="absolute inline-block w-full h-3 mt-2 -ml-2 bg-primary"></span>
@@ -8,15 +8,15 @@
       </h2>
       <!-- End Title -->
 
-      <div class="flex space-x-6 mt-10">
+      <div class="flex flex-col-reverse lg:flex-row lg:space-x-6 mt-10">
         <!-- Start Articles list -->
-        <div class="w-2/3 space-y-4">
+        <div class="lg:w-2/3 space-y-4 mt-6 lg:mt-0 lg:mb-0">
           <BlogArticleSnippet v-for="article in articles" :key="article.id" :article="article" />
         </div>
         <!-- End Articles list -->
 
         <!-- Start Search -->
-        <div class="w-1/3">
+        <div class="lg:w-1/3">
           <BlogSearch />
         </div>
         <!-- End Search -->
