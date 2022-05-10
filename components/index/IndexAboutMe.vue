bfe071d61f99037aa1ffbea77c68fc2b863e0fcd
Mohamed Imrane Chehabi <38333092+mohamedimrane@users.noreply.github.com>
2022-05-10 21:12:08 +0100

    fix: :bug: Makes IndexAboutMe fully responsive

--- components/index/IndexAboutMe.vue
+++ components/index/IndexAboutMe.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container mt-30 lg:mt-60 grid grid-cols-2 gap-x-20">
+  <div class="container mt-30 lg:mt-60 md:grid grid-cols-2 gap-x-20">
     <div>
       <div class="font-extrabold">
         <h1 class="text-6xl">Mohamed Imrane Chehabi</h1>
@@ -8,6 +8,9 @@
 
       <p class="mt-6 text-lg text-secondary-light">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas dolore ullam maiores odit explicabo dolorum velit adipisci officiis quis at.</p>
     </div>
-    <img src="~/assets/illustrations/developer.svg"/>
+
+    <div class="hidden md:block">
+      <img src="~/assets/illustrations/developer.svg"/>
+    </div>
   </div>
 </template>
