40aa8af3624021325c6c25cebf4fe9b968c16600
Mohamed Imrane Chehabi <38333092+mohamedimrane@users.noreply.github.com>
2022-08-22 10:34:43 -0400

    fix: :pencil2: Renames the site's title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'test-nuxt',
+    title: 'Mohamed Imrane Chehabi',
     htmlAttrs: {
       lang: 'en'
     },
